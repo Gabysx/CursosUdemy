b55a27162ae1a3fb124b1df99c9f0b767f4dfdfc
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-06-22 20:51:03 -0300

    destructuring

--- JavaScript/fundamentos/destructuring2.js
+++ JavaScript/fundamentos/destructuring2.js
@@ -1,4 +1,9 @@
 const [a] = [10]
 console.log(a)
 
-const [n1, n2, n3, n5, n6 = 0 ] = [10, 20, 30, 40, 50]
+const [n1, , n3, , n5, n6 = 0 ] = [10, 20, 30 ]
+
+console.log(n1,n3,n5, n6)
+
+const [, [, nota]]= [[ , 6, 41], [9, 8, 9]]
+console.log(nota)
