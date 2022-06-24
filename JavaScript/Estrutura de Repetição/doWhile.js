78e1a5d70640fc8408bb9f77690b1f8b8cccba0a
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-06-23 22:15:45 -0300

    doWhile

---
+++ JavaScript/Estrutura de Repetição/doWhile.js
@@ -0,0 +1,15 @@
+function inteiroAleatorio(min, max) {
+  const valor = Math.random() * (max - min) + min 
+  return Math.floor(valor)
+}
+
+let opcao 
+
+ do{
+  opcao = inteiroAleatorio(-1 , 10)
+  console.log(`Opção escolhida foi ${opcao}`)
+}while (opcao != -1)
+
+console.log('Até a próxima !!')
+
+// quantidade indeterminada 
