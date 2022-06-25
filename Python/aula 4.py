54e8b12fb83272184dd0f5ad308349971e107260
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-06-25 12:29:26 -0300

    If else

---
+++ Python/aula 4.py
@@ -0,0 +1,12 @@
+"""
+Condições IF , Elif, ELSE - Aula 4
+"""
+if True:
+
+    print('Verdadeiro')
+    #
+    # num_1 = 2
+    # num_2 = 3
+    #
+    # print(num_1 + num_2)
+print('A minha expressação não é verdadeira ! ')
